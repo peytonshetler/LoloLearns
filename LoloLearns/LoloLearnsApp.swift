a31bff5ec89c18cc97fe8de4b0c90dd80de6e06e
Peyton Shetler <peyton.shetler@gmail.com>
2022-06-12 10:53:09 -0500

    Initial Commit

---
+++ LoloLearns/LoloLearnsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoloLearnsApp.swift
+//  LoloLearns
+//
+//  Created by Peyton Shetler on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LoloLearnsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
